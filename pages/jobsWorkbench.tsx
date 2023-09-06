0b1f0820ed595d5cb7713f79353b98d257ea197f
Stan Chen <43565411+stancsz@users.noreply.github.com>
2023-09-05 23:50:55 -0600

    Enhance JobsWorkbench UI with improved layout and additional features

--- pages/jobsWorkbench.tsx
+++ pages/jobsWorkbench.tsx
@@ -1,98 +1,120 @@
 import React, { useEffect, useState } from 'react';
-import { db } from '../utils/firebase';  // Your existing Firestore setup
+import { useRouter } from 'next/router';
+import { db } from '../utils/firebase';
 import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from 'firebase/firestore';
-import styles from '@/styles/Home.module.css';
 
 interface JobData {
-  id: string;
-  name: string;
-  description: string;
-  analysis?: string;
-  rewrittenResume?: string;
+    id: string;
+    name: string;
+    description: string;
+    analysis?: string;
+    rewrittenResume?: string;
 }
 
 export default function JobsWorkbench() {
-  const [jobList, setJobList] = useState<JobData[]>([]);
-  const [selectedJob, setSelectedJob] = useState<JobData | null>(null);
-  const [showForm, setShowForm] = useState(false);
-  const [jobName, setJobName] = useState("");
-  const [jobDescription, setJobDescription] = useState("");
+    const [jobList, setJobList] = useState<JobData[]>([]);
+    const [selectedJob, setSelectedJob] = useState<JobData | null>(null);
+    const [showForm, setShowForm] = useState(false);
+    const [jobName, setJobName] = useState("");
+    const [jobDescription, setJobDescription] = useState("");
+    const router = useRouter();
 
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, 'jobs'), (snapshot) => {
-      const newJobs: JobData[] = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      } as JobData));
-      setJobList(newJobs);
-    });
+    useEffect(() => {
+        const unsub = onSnapshot(collection(db, 'jobs'), (snapshot) => {
+            const newJobs: JobData[] = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            } as JobData));
+            setJobList(newJobs);
+        });
 
-    return () => {
-      unsub();
+        return () => {
+            unsub();
+        };
+    }, []);
+
+    const handleReturn = () => {
+        router.push('/dashboard');
     };
-  }, []);
 
-  const handleAddNew = () => {
-    setJobName("");
-    setJobDescription("");
-    setSelectedJob(null);
-    setShowForm(true);
-  };
+    const handleAddNew = () => {
+        setJobName("");
+        setJobDescription("");
+        setSelectedJob(null);
+        setShowForm(true);
+    };
 
-  const handleRemove = async () => {
-    if (selectedJob) {
-      await deleteDoc(doc(db, 'jobs', selectedJob.id));
+    const handleRemove = async () => {
+        if (selectedJob) {
+            await deleteDoc(doc(db, 'jobs', selectedJob.id));
+        }
+    };
+
+    const handleSave = async () => {
+        if (selectedJob) {
+            await updateDoc(doc(db, 'jobs', selectedJob.id), { name: jobName, description: jobDescription });
+        } else {
+            await addDoc(collection(db, 'jobs'), { name: jobName, description: jobDescription });
+        }
+        setShowForm(false);
+    };
+
+    const handleEdit = (job: JobData) => {
+        setSelectedJob(job);
+        setJobName(job.name);
+        setJobDescription(job.description);
+        setShowForm(true);
+    };
+
+    return (
+        <div className="container">
+          <h1>Jobs Workbench</h1>
+          <button className="btn btn-info mb-3" onClick={() => router.push('/dashboard')}>Return</button>
+          <p>Manage your job descriptions here.</p>
+          
+          <div className="row">
+            <div className="col-4">
+              <div className="d-flex justify-content-between mb-3">
+                <button className="btn btn-secondary" onClick={handleAddNew}>Add New</button>
+                <button className="btn btn-danger" onClick={handleRemove}>Remove</button>
+              </div>
+              <ul className="list-group">
+                {jobList.map((job) => (
+                  <li key={job.id} className="list-group-item" onClick={() => handleEdit(job)}>
+                    {job.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+    
+            <div className="col-8">
+              {showForm && (
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control mb-3"
+                    placeholder="Job Name"
+                    value={jobName}
+                    onChange={(e) => setJobName(e.target.value)}
+                  />
+                  <textarea
+                    className="form-control mb-3"
+                    placeholder="Job Description"
+                    rows={4}
+                    value={jobDescription}
+                    onChange={(e) => setJobDescription(e.target.value)}
+                  />
+                  <input type="text" className="form-control mb-3" placeholder="Job Analysis"/>
+                  <div className="d-flex justify-content-between mb-3">
+                    <button className="btn btn-primary">Analyze</button>
+                    <button className="btn btn-primary">Rewrite Resume</button>
+                    <button className="btn btn-warning">Copy</button>
+                    <button className="btn btn-success" onClick={handleSave}>Save</button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      );
     }
-  };
-
-  const handleSave = async () => {
-    if (selectedJob) {
-      await updateDoc(doc(db, 'jobs', selectedJob.id), { name: jobName, description: jobDescription });
-    } else {
-      await addDoc(collection(db, 'jobs'), { name: jobName, description: jobDescription });
-    }
-    setShowForm(false);
-  };
-
-  const handleEdit = (job: JobData) => {
-    setSelectedJob(job);
-    setJobName(job.name);
-    setJobDescription(job.description);
-    setShowForm(true);
-  };
-
-  return (
-    <div className={`d-flex justify-content-center align-items-center vh-100 ${styles.unselectable}`}>
-      <div className="text-center">
-        <button className="btn btn-secondary mb-2" onClick={handleAddNew}>Add New</button>
-        <button className="btn btn-danger mb-2" onClick={handleRemove}>Remove</button>
-
-        {showForm && (
-          <form>
-            <input
-              type="text"
-              placeholder="Job Name"
-              value={jobName}
-              onChange={(e) => setJobName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Job Description"
-              value={jobDescription}
-              onChange={(e) => setJobDescription(e.target.value)}
-            />
-            <button className="btn btn-success" type="button" onClick={handleSave}>Save</button>
-          </form>
-        )}
-
-        <ul>
-          {jobList.map((job) => (
-            <li key={job.id} onClick={() => handleEdit(job)}>
-              {job.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
